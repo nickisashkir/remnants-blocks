1fff784b759bf86fef3f199b89e6a73b1e48f18c
nickisashkir <5299975+nickisashkir@users.noreply.github.com>
2026-04-18 01:37:22 -0700

    Add tags, stonecutting recipes, grass tintindex, creative tab
    - Add block tags: mineable/pickaxe (concrete, amethyst, obsidian),
      mineable/shovel (dirt types), needs_diamond_tool (obsidian),
      slabs tag (all 23 types)
    - Add item slabs tag for all 23 types
    - Add 19 stonecutting recipes for stone-type slabs (1 block = 2 slabs)
    - Fix grass slab item model: custom model with tintindex on top face
      and grass_block_side_overlay for proper biome coloring in inventory
    - Add Polymer creative tab so slabs appear in creative menu
    - Remove unused _double model generation

--- src/main/java/net/remnants/moreslabs/MoreSlabs.java
+++ src/main/java/net/remnants/moreslabs/MoreSlabs.java
@@ -2,20 +2,38 @@ package net.remnants.moreslabs;
 
 import com.mojang.logging.LogUtils;
 import net.remnants.moreslabs.registry.SlabRegistry;
+import eu.pb4.polymer.core.api.item.PolymerCreativeModeTabUtils;
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Items;
 import org.slf4j.Logger;
 
 public class MoreSlabs implements ModInitializer {
     public static final String MODID = "remnants_blocks";
     public static final Logger LOGGER = LogUtils.getLogger();
 
+    public static final CreativeModeTab CREATIVE_TAB = new CreativeModeTab.Builder(CreativeModeTab.Row.TOP, -1)
+            .title(Component.literal("Remnants Blocks").withStyle(ChatFormatting.GOLD))
+            .icon(Items.STONE_SLAB::getDefaultInstance)
+            .displayItems((parameters, output) -> {
+                for (SlabTypes type : SlabTypes.values()) {
+                    output.accept(SlabRegistry.getItem(type));
+                }
+            })
+            .build();
+
     @Override
     public void onInitialize() {
         SlabRegistry.register();
+        PolymerCreativeModeTabUtils.registerPolymerCreativeModeTab(
+                Identifier.fromNamespaceAndPath(MODID, "blocks"), CREATIVE_TAB);
         PolymerResourcePackUtils.addModAssets(MODID);
         PolymerResourcePackUtils.markAsRequired();
         ResourcePackGenerator.register();
-        LOGGER.info("Remnants More Slabs loaded - {} slab types registered", SlabTypes.values().length);
+        LOGGER.info("Remnants Building Blocks loaded - {} slab types registered", SlabTypes.values().length);
     }
 }
--- src/main/java/net/remnants/moreslabs/ResourcePackGenerator.java
+++ src/main/java/net/remnants/moreslabs/ResourcePackGenerator.java
@@ -1,6 +1,7 @@
 package net.remnants.moreslabs;
 
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
+import eu.pb4.polymer.resourcepack.api.ResourcePackBuilder;
 
 /**
  * Generates resource pack assets programmatically during pack creation.
@@ -8,6 +9,7 @@ import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
  * Generated assets:
  * - Invisible petrified_oak_slab blockstate (base block clients see)
  * - Slab block models (bottom/top variants per type)
+ * - Custom grass slab model with tintindex + side overlay
  * - Item definitions (what players see in inventory)
  */
 public class ResourcePackGenerator {
@@ -15,6 +17,7 @@ public class ResourcePackGenerator {
     public static void register() {
         PolymerResourcePackUtils.RESOURCE_PACK_CREATION_EVENT.register(builder -> {
             generateInvisibleSlab(builder);
+            generateGrassSlabTemplates(builder);
             for (SlabTypes type : SlabTypes.values()) {
                 generateBlockModels(builder, type);
                 generateItemDefinition(builder, type);
@@ -22,7 +25,7 @@ public class ResourcePackGenerator {
         });
     }
 
-    private static void generateInvisibleSlab(eu.pb4.polymer.resourcepack.api.ResourcePackBuilder builder) {
+    private static void generateInvisibleSlab(ResourcePackBuilder builder) {
         builder.addStringData("assets/minecraft/models/block/more_slabs_invisible.json", """
                 {
                   "textures": {
@@ -45,8 +48,104 @@ public class ResourcePackGenerator {
                 """);
     }
 
-    private static void generateBlockModels(eu.pb4.polymer.resourcepack.api.ResourcePackBuilder builder, SlabTypes type) {
+    /**
+     * Generates custom grass slab templates with tintindex on top face and
+     * side overlay for proper biome coloring in the item model.
+     */
+    private static void generateGrassSlabTemplates(ResourcePackBuilder builder) {
+        String ns = MoreSlabs.MODID;
+
+        // Bottom grass slab with tintindex + side overlay
+        builder.addStringData("assets/" + ns + "/models/block/grass_block_slab.json", GRASS_SLAB_BOTTOM);
+
+        // Top grass slab
+        builder.addStringData("assets/" + ns + "/models/block/grass_block_slab_top.json", GRASS_SLAB_TOP);
+    }
+
+    // Custom grass slab model (bottom) with tintindex on top face and side overlay
+    private static final String GRASS_SLAB_BOTTOM = """
+            {
+              "parent": "minecraft:block/block",
+              "textures": {
+                "bottom": "minecraft:block/dirt",
+                "top": "minecraft:block/grass_block_top",
+                "side": "minecraft:block/grass_block_side",
+                "overlay": "minecraft:block/grass_block_side_overlay",
+                "particle": "minecraft:block/dirt"
+              },
+              "elements": [
+                {
+                  "from": [0, 0, 0],
+                  "to": [16, 8, 16],
+                  "faces": {
+                    "down":  { "uv": [0, 0, 16, 16], "texture": "#bottom", "cullface": "down" },
+                    "up":    { "uv": [0, 0, 16, 16], "texture": "#top", "cullface": "up", "tintindex": 0 },
+                    "north": { "uv": [0, 8, 16, 16], "texture": "#side", "cullface": "north" },
+                    "south": { "uv": [0, 8, 16, 16], "texture": "#side", "cullface": "south" },
+                    "west":  { "uv": [0, 8, 16, 16], "texture": "#side", "cullface": "west" },
+                    "east":  { "uv": [0, 8, 16, 16], "texture": "#side", "cullface": "east" }
+                  }
+                },
+                {
+                  "from": [0, 0, 0],
+                  "to": [16, 8, 16],
+                  "faces": {
+                    "north": { "uv": [0, 8, 16, 16], "texture": "#overlay", "cullface": "north", "tintindex": 0 },
+                    "south": { "uv": [0, 8, 16, 16], "texture": "#overlay", "cullface": "south", "tintindex": 0 },
+                    "west":  { "uv": [0, 8, 16, 16], "texture": "#overlay", "cullface": "west", "tintindex": 0 },
+                    "east":  { "uv": [0, 8, 16, 16], "texture": "#overlay", "cullface": "east", "tintindex": 0 }
+                  }
+                }
+              ]
+            }
+            """;
+
+    // Custom grass slab model (top) with tintindex
+    private static final String GRASS_SLAB_TOP = """
+            {
+              "parent": "minecraft:block/block",
+              "textures": {
+                "bottom": "minecraft:block/dirt",
+                "top": "minecraft:block/grass_block_top",
+                "side": "minecraft:block/grass_block_side",
+                "overlay": "minecraft:block/grass_block_side_overlay",
+                "particle": "minecraft:block/dirt"
+              },
+              "elements": [
+                {
+                  "from": [0, 8, 0],
+                  "to": [16, 16, 16],
+                  "faces": {
+                    "down":  { "uv": [0, 0, 16, 16], "texture": "#bottom", "cullface": "down" },
+                    "up":    { "uv": [0, 0, 16, 16], "texture": "#top", "cullface": "up", "tintindex": 0 },
+                    "north": { "uv": [0, 0, 16, 8], "texture": "#side", "cullface": "north" },
+                    "south": { "uv": [0, 0, 16, 8], "texture": "#side", "cullface": "south" },
+                    "west":  { "uv": [0, 0, 16, 8], "texture": "#side", "cullface": "west" },
+                    "east":  { "uv": [0, 0, 16, 8], "texture": "#side", "cullface": "east" }
+                  }
+                },
+                {
+                  "from": [0, 8, 0],
+                  "to": [16, 16, 16],
+                  "faces": {
+                    "north": { "uv": [0, 0, 16, 8], "texture": "#overlay", "cullface": "north", "tintindex": 0 },
+                    "south": { "uv": [0, 0, 16, 8], "texture": "#overlay", "cullface": "south", "tintindex": 0 },
+                    "west":  { "uv": [0, 0, 16, 8], "texture": "#overlay", "cullface": "west", "tintindex": 0 },
+                    "east":  { "uv": [0, 0, 16, 8], "texture": "#overlay", "cullface": "east", "tintindex": 0 }
+                  }
+                }
+              ]
+            }
+            """;
+
+    private static void generateBlockModels(ResourcePackBuilder builder, SlabTypes type) {
         String id = type.getSlabId();
+
+        // Grass slab uses custom templates generated above -- skip standard generation
+        if (type == SlabTypes.GRASS_BLOCK) {
+            return;
+        }
+
         String bottom = type.getBottomTexturePath();
         String top = type.getTopTexturePath();
         String side = type.getSideTexturePath();
@@ -56,12 +155,9 @@ public class ResourcePackGenerator {
 
         builder.addStringData("assets/" + MoreSlabs.MODID + "/models/block/" + id + "_top.json",
                 slabModel("minecraft:block/slab_top", bottom, top, side));
-
-        builder.addStringData("assets/" + MoreSlabs.MODID + "/models/block/" + id + "_double.json",
-                cubeModel(bottom, top, side));
     }
 
-    private static void generateItemDefinition(eu.pb4.polymer.resourcepack.api.ResourcePackBuilder builder, SlabTypes type) {
+    private static void generateItemDefinition(ResourcePackBuilder builder, SlabTypes type) {
         String id = type.getSlabId();
         String modelRef = MoreSlabs.MODID + ":block/" + id;
 
@@ -104,28 +200,5 @@ public class ResourcePackGenerator {
                   }
                 }
                 """.formatted(parent, bottom, top, side);
-    }
-
-    private static String cubeModel(String bottom, String top, String side) {
-        if (bottom.equals(top) && top.equals(side)) {
-            return """
-                    {
-                      "parent": "minecraft:block/cube_all",
-                      "textures": {
-                        "all": "%s"
-                      }
-                    }
-                    """.formatted(bottom);
-        }
-        return """
-                {
-                  "parent": "minecraft:block/cube_bottom_top",
-                  "textures": {
-                    "bottom": "%s",
-                    "top": "%s",
-                    "side": "%s"
-                  }
-                }
-                """.formatted(bottom, top, side);
     }
 }
